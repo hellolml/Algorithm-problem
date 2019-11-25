f838e1918b1c9008ef0940e2d1c1416fe54b1399
hellolml <2581154176@qq.com>
2019-11-25 20:27:32 +0800

    百万富翁

---
+++ November/Millionaire.java
@@ -0,0 +1,9 @@
+package November;
+
+public class Millionaire {
+    public static void main(String args[]){
+            int a = 10*30;
+            int b = (int)Math.pow(2, 30)-1;
+            System.out.println(a+" "+b);
+    }
+}
