7183573fd117487af28c440c22326add6cdc3e39
hellolml <2581154176@qq.com>
2020-02-19 18:37:59 +0800

    排序平方数

---
+++ January/SortedSquares.java
@@ -0,0 +1,13 @@
+package January;
+
+import java.util.Arrays;
+
+public class SortedSquares {
+    public int[] sortedSquares(int[] A) {
+        for (int i = 0; i <A.length ; i++) {
+            A[i] = (int) Math.pow(A[i],2 );
+        }
+        Arrays.sort(A);
+        return A;
+    }
+}
