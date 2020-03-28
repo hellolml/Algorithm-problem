f4dec62a1069d3cc953dfe6cfe2b24cc6c7376ca
hellolml <2581154176@qq.com>
2020-03-28 08:54:01 +0800

    money交换

---
+++ March/ChangeMoney.java
@@ -0,0 +1,9 @@
+package March;
+
+public class ChangeMoney{
+    public static void main(String [] args){
+        long mosheng = 10*30;
+        long furen = (long) Math.pow(2,30);
+        System.out.println(mosheng+" "+furen);
+    }
+}
